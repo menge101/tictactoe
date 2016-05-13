0a9224200f85c5602ac9c3efd0eb9ed82d314e2c
junehoe <briansung.09@gmail.com>
2016-05-13 11:56:15 -0700

    Create message class
    Created a message class to handle all of the messages. In addition, a
    message spec file was created to test the outputs of each message.

---
+++ lib/message.rb
@@ -0,0 +1,18 @@
+class Message
+
+  def player_move(name, letter)
+    return "#{name} (#{letter}), select a space:"
+  end
+
+  def unavailable_cell
+    return "Cell is unavailable. Try again"
+  end
+
+  def game_over(game_state, name)
+    if game_state == :winner
+      return "#{name} won!"
+    elsif game_state == :draw
+      return "Cat's game!"
+    end
+  end
+end
