f1939332526397b07ff6439894c9ae213251fc3d
junehoe <briansung.09@gmail.com>
2016-05-16 11:49:09 -0700

    Swap implementation between classes
    Swapped methods from each class.

--- lib/board.rb
+++ lib/board.rb
@@ -6,14 +6,6 @@ class Board
     @grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
   end
 
-  def display_board
-    puts " #{grid[0][0]} | #{grid[0][1]} | #{grid[0][2]} "
-    puts "-----------"
-    puts " #{grid[1][0]} | #{grid[1][1]} | #{grid[1][2]} "
-    puts "-----------"
-    puts " #{grid[2][0]} | #{grid[2][1]} | #{grid[2][2]} "
-  end
-
   def write_cell(x, y, value)
     grid[x][y] = value
   end
@@ -36,6 +28,25 @@ class Board
 
   def diagonals
     top_left_bottom_right_diagonal || top_right_bottom_left_diagonal
+  end
+
+  def number_to_coordinates(cell_number)
+    mapping = {
+      1 => [0, 0],
+      2 => [0, 1],
+      3 => [0, 2],
+      4 => [1, 0],
+      5 => [1, 1],
+      6 => [1, 2],
+      7 => [2, 0],
+      8 => [2, 1],
+      9 => [2, 2]
+    }
+    mapping[cell_number]
+  end
+
+  def valid_cell_number(cell_number)
+    cell_number >= 1 && cell_number <= 9
   end
 
 
--- lib/game.rb
+++ lib/game.rb
@@ -1,19 +1,67 @@
-require_relative 'tictactoe.rb'
-require_relative 'player.rb'
-require_relative 'board.rb'
-require_relative 'message.rb'
+class Game
 
-puts "Enter player 1 name:"
-player1_name = gets.chomp
-puts "Enter player 2 name:"
-player2_name = gets.chomp
-player1 = Player.new({name: player1_name, letter: 'X'})
-player2 = Player.new({name: player2_name, letter: 'O'})
-players = [player1, player2]
+  attr_reader :board, :players, :ui, :current_player, :next_player
 
-board = Board.new
+  def initialize(board, players, ui)
+    @board = board
+    @players = players
+    @ui = ui
+    @current_player, @next_player = players.shuffle
+    @turn_count = 0
+  end
 
-message = Message.new
+  def play
+    until win? or draw?
+      ui.display_board
+      ui.player_move_message(current_player.name, current_player.letter)
+      player_move
+    end
+    game_over
+  end
 
-game = TicTacToe.new(board, players, message)
-game.game
+  def switch_players
+    @current_player, @next_player = @next_player, @current_player
+  end
+
+  def game_over
+    ui.display_board
+    if win?
+      ui.winner(next_player.name)
+    #elsif draw?
+    else
+      ui.draw
+    end
+  end
+
+
+  private
+
+  def player_move
+    x, y = get_coordinates
+    until board.cell_available?(x, y)
+      ui.unavailable_cell_message
+      x, y = get_coordinates
+    end
+    board.write_cell(x, y, current_player.letter)
+    if !win? or !draw?
+      switch_players
+      @turn_count += 1
+    end
+  end
+
+  def get_coordinates(cell_number = ui.get_move)
+    until board.valid_cell_number(cell_number)
+      ui.invalid_cell_number_message
+      cell_number = ui.get_move
+    end
+    board.number_to_coordinates(cell_number)
+  end
+
+  def win?
+    board.horizontals || board.verticals || board.diagonals
+  end
+
+  def draw?
+    @turn_count == 9
+  end
+end
